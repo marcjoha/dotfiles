81e6d9a9f9877adf6bbb5f633e003cb0bb27388f
Marcus Johansson <majohansson@gmail.com>
2019-03-12 09:38:12 +0100

    new

--- hyper.js
+++ hyper.js
@@ -140,7 +140,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-solarized', 'hyperterm-working-directory', 'hyper-quit'],
+  plugins: ['hyper-solarized-light', 'hyperterm-working-directory', 'hyper-quit'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
